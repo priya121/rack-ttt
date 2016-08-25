f41a991fafb3668e77a3f754050cf41a5b255e18
priya121 <priya121@github.com>
2016-08-25 13:28:58 +0100

    Changes tests for controller mark position.

--- spec/controller_spec.rb
+++ spec/controller_spec.rb
@@ -9,22 +9,17 @@ describe Controller do
     expect(controller.mark_board("---------", 6)).to eq "-----X---" 
   end
 
-  it 'marks multiple positions on board' do
+  it 'marks a position on an already marked board' do
     controller = Controller.new
-    mark_board_positions(controller, "---------", [1, 2, 3, 4])
-    expect(controller.game.board.cells.join).to eq "XOXO-----" 
+    controller.mark_board("---------", 1)
+    controller.mark_board("X--------", 2)
+    controller.mark_board("XO-------", 9)
+    expect(controller.game.board.cells.join).to eq "XO------X" 
   end
 
   it 'does not mark a taken position' do
     controller = Controller.new
     expect(controller.mark_board("X----O---", 6)).to eq "X----O---" 
     expect(controller.mark_board("---OXX---", 6)).to eq "---OXX---" 
-  end
-
-  def mark_board_positions(controller, board, positions)
-    positions.each do |position|
-      controller.mark_board(board, position)
-    end
-    board
   end
 end
